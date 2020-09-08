0a2ab8f80d870a5f4aa384f76b3d937411015a09
Brian Zoetewey <brian.zoetewey@cru.org>
2020-09-08 15:35:37 -0400

    Read Parameters from the response

--- handlers/keypalive.js
+++ handlers/keypalive.js
@@ -7,8 +7,8 @@ export const handler = async (lambdaEvent) => {
     const ssm = new SSM({ region: 'us-east-1' })
     const apiKeyPaths = process.env.API_KEY_PATHS.split(',')
     if (apiKeyPaths.length > 0) {
-      const parameters = await ssm.getParameters({ Names: apiKeyPaths, WithDecryption: true }).promise()
-      await Promise.allSettled(parameters.map(parameter => {
+      const response = await ssm.getParameters({ Names: apiKeyPaths, WithDecryption: true }).promise()
+      await Promise.allSettled(response.Parameters.map(parameter => {
         const client = new Client({ orgUrl: process.env.OKTA_ORG_URL, token: parameter.Value })
         return client.listUsers({ q: 'John', limit: 1 })
       }))
